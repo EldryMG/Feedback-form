7f2f4093a0d0bd6b7ebcc8d0d4a0d9d54ae31749
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-09-25 14:18:25 -0500

    tested and added an Alert for invalid inputs

--- README.md
+++ README.md
@@ -8,9 +8,12 @@ This is my version of Prime Digital Academy's Daily Reflections Form.  This was 
 [x]-update README. ~15 mins, but ongoing.
 [x]-Create the form components; "/feeling", "/understanding", "/support", "/comments" ~1 hour
 [x]-Link together each blank page, importing from 'react-router-dom' HashRouter. ~45 mins, but these will likely need to be tweaked once data is being passed around.
-[]-Complete Form on "/feeling", pass data to Redux.~1 hour
-[]-Receive data from "/feeling".
-[]-Repeat for "/understanding".
+[x]-Complete Form on "/feeling", pass data to Redux.~1 hour
+[x]-Receive data from "/feeling".
+[x]-Repeat for "/understanding".
+[] - test outputs and inputs into Redux.
+[] - Repeat for final 2 pages.
+
 
 Aha moments: 
     -I did 2 different styles of the first page. The first was a form that had an input field that pushed the user input via 'event.target.value' into a useState pair.  on Submit, another function would be called that would use the useState value and dispatch it to Redux.
--- src/components/Feeling/Feeling.jsx
+++ src/components/Feeling/Feeling.jsx
@@ -7,26 +7,32 @@ import { useDispatch } from 'react-redux'; //only useDispatch is used.
 function Feeling() {
     //dispatch to Redux
     const dispatch = useDispatch();
+
     //Set history to change pages.
     const history = useHistory();
+
     //Set a variable to push value into.
     const [feelingsValue, setFeelingsValue] = useState('');
 
     const addValue = (event) => {
         event.preventDefault()
-        console.log('in addValue');
-        const action = {
-            type: 'SET_FEELING',
-            result: feelingsValue,
-        };
-        dispatch(action);
-        history.push('/understanding');
+        console.log(feelingsValue);
+        if (feelingsValue > 5 || feelingsValue < 1) {
+            alert('Please pick a number between 1 and 5');
+        } else {
+            const action = {
+                type: 'SET_FEELING',
+                result: feelingsValue,
+            }
+            dispatch(action);
+            history.push('/understanding');
+        }
     }
 
     return (
         <>
             <h2>How are you feeling today?</h2>
-{/* Click submit, call addValue which will send data to Redux */}
+            {/* Click submit, call addValue which will send data to Redux */}
             <form onSubmit={(event) => addValue(event)}>
                 <input value={feelingsValue}
                     type="number"
