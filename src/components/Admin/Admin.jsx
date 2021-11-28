d8bbd1f895eac9d24bcb88dc111a660dbd817bdb
Franck Bushbaum <franck.bushbaum@yahoo.com>
2021-11-28 15:59:40 -0600

    Table styling continued, table centered properly.

--- src/components/Admin/Admin.jsx
+++ src/components/Admin/Admin.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 import React from 'react';
 import AdminItem from '../AdminItem/AdminItem';
+import "./Admin.css"
 import {
     Typography,
     Paper,
@@ -12,13 +13,13 @@ import {
     TableRow,
     TableCell,
     TableBody,
-  } from "@mui/material";
+} from "@mui/material";
 
 
-function Admin(){
+function Admin() {
     const [adminData, setAdminData] = useState([]);
-//get Client-side
-    const getServerInfo = () =>{
+    //get Client-side
+    const getServerInfo = () => {
         axios({
             method: 'GET',
             url: '/feedback'
@@ -29,12 +30,12 @@ function Admin(){
             console.log('There was an error getting data back', error)
         })
     }
-//put Client-side
-    const markFlagged = (id) =>{
+    //put Client-side
+    const markFlagged = (id) => {
         console.log('in markFlagged', id)
         axios({
             method: 'PUT',
-            url:`/feedback/` + id,
+            url: `/feedback/` + id,
         }).then(response => {
             console.log('response.data is', id)
             getServerInfo();
@@ -51,34 +52,38 @@ function Admin(){
     return (
         <>
             <h3>Recent Submissions</h3>
-             {/* <p>{JSON.stringify(adminData[0].flagged)}</p> */}
-             <TableContainer>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Id</TableCell>
-                        <TableCell>Feeling</TableCell>
-                        <TableCell>Understanding</TableCell>
-                        <TableCell>Support</TableCell>
-                        <TableCell>Comments</TableCell>
-                        <TableCell>Flagged</TableCell>
-                        <TableCell>Timestamp</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {adminData.map(student => {
-                        return (<AdminItem key={student.id}
-                                           id={student.id}
-                                           feeling={student.feeling}
-                                           understanding={student.understanding}
-                                           support={student.support}
-                                           comments={student.comments}
-                                           flagged={student.flagged}
-                                           date={student.date} />);                   
-                    })}
-                </TableBody>
-            </Table>
-            </TableContainer>
+            {/* <p>{JSON.stringify(adminData[0].flagged)}</p> */}
+            {/* <Paper elevation={4} className="adminPaper"> */}
+            <div className="table-container">
+                <TableContainer component={Paper} elevation={4} sx={{ margin: 11, padding: 7, maxWidth: 850, textAlign: "center"}} >
+                    <Table >
+                        <TableHead>
+                            <TableRow>
+                                <TableCell>Id</TableCell>
+                                <TableCell>Feeling</TableCell>
+                                <TableCell>Understanding</TableCell>
+                                <TableCell>Support</TableCell>
+                                <TableCell>Comments</TableCell>
+                                <TableCell>Flagged</TableCell>
+                                <TableCell>Timestamp</TableCell>
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {adminData.map(student => {
+                                return (<AdminItem key={student.id}
+                                    id={student.id}
+                                    feeling={student.feeling}
+                                    understanding={student.understanding}
+                                    support={student.support}
+                                    comments={student.comments}
+                                    flagged={student.flagged}
+                                    date={student.date} />);
+                            })}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+            </div>
+            {/* </Paper> */}
 
         </>
 
